d8040fa91a8ec98004967618837976908188533d
tn1031 <ttt.nakamura1031@gmail.com>
2017-11-06 12:25:15 +0900

    slack notifier

---
+++ slack_report.py
@@ -0,0 +1,63 @@
+import hashlib
+import os
+import slackweb
+import yaml
+from google.cloud import storage
+from chainer.training import extension
+
+def slack_report(keys, hook, channel, config_path=None):
+    @extension.make_extension(trigger=(1, 'epoch'), priority=0)
+    def slack_report(trainer):
+        _slack_report(trainer, keys, hook, channel, config_path)
+
+    return slack_report
+
+def _slack_report(trainer, keys, hook, channel, config_path):
+    slack = slackweb.Slack(url=hook)
+    updater = trainer.updater
+    model_name = updater._optimizers['main'].target.__class__.__name__
+    log_report = trainer.get_extension('LogReport')
+    current_log = log_report.log[-1]
+
+    attachments = list()
+
+    fields = list()
+    color = 'good'
+    for k in keys:
+        if k in current_log:
+            fields.append({'title': k, 'value': current_log[k], 'short': True})
+            if not (isinstance(current_log[k], float) or isinstance(current_log[k], int)):
+                color = 'danger'
+                # mention
+    attachments.append(
+        {'pretext': model_name, 'color': color, 'fields': fields})
+
+    if config_path is not None:
+        config = yaml.load(open(config_path))
+        client = storage.Client.from_service_account_json(
+            config['key-file'], project=config['project'])
+        bucket = client.get_bucket(config['bucket'])
+
+        plot_reports = [v.extension for k, v in trainer._extensions.items() if 'PlotReport' in k]
+        if len(plot_reports) > 0:
+            for pr in plot_reports:
+                name = pr._file_name
+                url = _upload_figure(name, trainer.out, bucket, config['figure-uri'])
+                attachments.append(
+                    {'color': color, 'image_url': url, 'fields': [{'value': name}]})
+
+    slack.notify(channel=channel,
+        text='Training Report from %s' % os.uname()[1],
+        attachments=attachments)
+
+def _upload_figure(name, out, bucket, figure_uri):
+    path = os.path.join(out, name)
+
+    with open(path, 'rb') as f:
+        chsum = hashlib.md5(f.read()).hexdigest()
+    dst = os.path.join(figure_uri, chsum + '.png')
+    blob = bucket.blob(dst)
+    blob.upload_from_filename(path)
+    blob.make_public()
+
+    return blob.public_url
---
+++ snapshot_transfer.py
@@ -0,0 +1,50 @@
+import os
+import shutil
+import tarfile
+import tempfile
+import yaml
+from datetime import datetime as dt
+from glob import glob
+from google.cloud import storage
+from chainer.training import extension
+
+
+def snapshot_transfer(keys, path):
+    @extension.make_extension(trigger=(1, 'epoch'), priority=-200)
+    def snapshot_transfer(trainer):
+        _snapshot_transfer(trainer, keys, path)
+
+    return snapshot_transfer
+
+
+def _snapshot_transfer(trainer, keys, config_path):
+    config = yaml.load(open(config_path))
+    gcs_client = storage.Client.from_service_account_json(
+        config['key-file'], project=config['project'])
+    bucket = gcs_client.get_bucket(config['bucket'])
+    model_name = trainer.updater._optimizers['main'].target.__class__.__name__
+
+    targets = [_get_latest_modified_object(trainer.out, k) for k in keys]
+
+    prefix = model_name + dt.now().strftime('%y%m%d%H%M') + '_'
+    with tempfile.TemporaryDirectory(prefix=prefix, dir=trainer.out) as tmp_path:
+        for f in filter(lambda x: x is not None, targets):
+            shutil.copyfile(f, os.path.join(tmp_path, os.path.basename(f)))
+        out_tar = tmp_path + '.tar.gz'
+        with tarfile.open(out_tar, mode='w:gz') as tar:
+            tar.add(tmp_path, arcname=os.path.basename(tmp_path))
+
+        dst = os.path.join(config['dst'], os.uname()[1], os.path.basename(out_tar))
+        blob = bucket.blob(dst)
+        blob.upload_from_filename(out_tar)
+
+        os.remove(out_tar)
+
+
+def _get_latest_modified_object(dirname, key):
+    target = os.path.join(dirname, '%s*' % key)
+    files = [(f, os.path.getmtime(f)) for f in glob(target)]
+    if len(files) == 0:
+        return
+    latest = sorted(files, key=lambda x: x[1])[-1]
+    return latest[0]
